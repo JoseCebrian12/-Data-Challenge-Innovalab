0dad42e0da2fcd07e6c7f8e87e5bd006020c4ef5
Jose Enrique Cebrian Baca <a20181041@pucp.edu.pe>
2024-08-14 01:13:27 -0500

    cambio de nombre

--- scripts/01_data_collection.R
+++
@@ -1,6 +0,0 @@
-# Definir la URL y la ruta donde se guardará el archivo
-url <- "http://www.datosabiertos.gob.pe/sites/default/files/datos_abiertos_vigilancia_dengue.csv"
-destfile <- "data/datos_abiertos_vigilancia_dengue.csv"
-
-# Descargar el archivo usando libcurl para evitar problemas de SSL
-download.file(url = url, destfile = destfile, method = "libcurl")
