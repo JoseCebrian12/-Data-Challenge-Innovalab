965f4b8a798e702af15b4ba2b25566a2cba92718
Jose Enrique Cebrian Baca <a20181041@pucp.edu.pe>
2024-08-14 01:18:02 -0500

    correccion del url

--- 2_scripts/01_data_collection.R
+++ 2_scripts/01_data_collection.R
@@ -1,4 +1,4 @@
-url <- "http://www.datosabiertos.gob.pe/sites/default/files/datos_abiertos_vigilancia_dengue.csv"
+url <- "https://www.datosabiertos.gob.pe/sites/default/files/datos_abiertos_vigilancia_dengue.csv"
 destfile <- "1_data/datos_abiertos_vigilancia_dengue.csv"
 
 # descargamos el archivo 
